71ff99b8b43b30e45743b529bc9f2b9af7837856
Philip Sole <philip.sole@gmail.com>
2020-03-26 22:55:13 +1300

    massaging for heroku

--- index.js
+++ index.js
@@ -44,8 +44,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-http.listen(3000, function(){
+http.listen(port, function(){
     console.log('listening on *:3000');
 });
