ec3f0f794999ec19b2d7d982af522db52d8ce0ce
Philip Sole <philip.sole@gmail.com>
2020-04-18 19:31:32 +1200

    working??? nah

--- index.js
+++ index.js
@@ -171,9 +171,10 @@ io.on('connection', function(socket){
     socket.on('join room', function(nickname, roomKey, acknowledge){
         playersList.push({id:socket.id, nickname:nickname, roomKey:roomKey});
         let allPlayers = playersList.filter(player => player.roomKey === roomKey);
+        let roomObject = roomsList.find(room => room.lock === roomKey);
 
         socket.join(roomKey);
-        acknowledge(allPlayers);
+        acknowledge(allPlayers, roomObject, dataMinigames);
         socket.to(roomKey).emit('new member', allPlayers);
     });
 
